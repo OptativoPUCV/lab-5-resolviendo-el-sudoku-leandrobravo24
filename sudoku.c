22de1564d4a682e24da964133a6fb27323d8c157
Your Name <you@example.com>
2025-05-13 13:08:38 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -74,21 +74,25 @@ int is_valid(Node* n){
 
 
 List* get_adj_nodes(Node* n){
-  List* list=createList();
-  for(size_t i=0;i<9;i++){
-    for(size_t j=0;j<9;j++){
+  List* list = create_list();
+  int i,j;
+  for(i=0;i<9;i++){
+    for(j=0;j<9;j++){
       if(n->sudo[i][j]==0){
-        for(size_t k=1;k<=9;k++){
-          Node* new=copy(n);
+        for(int k=1;k<=9;k++){
+          Node* new = copy(n);
           new->sudo[i][j]=k;
           if(is_valid(new)){
-            pushBack(list, new);
+            add(list, new);
+          }else{
+            free(new);
           }
         }
         return list;
       }
     }
   }
+  return list;
 }
 
 int is_final(Node* n){
